181cb2f24e969c578be60749ae0680909297a1fd
Dan Kalinin <daniil551@hotmail.com>
2018-06-20 18:06:32 +0300

    Abstract methods

--- Helpers/HLPArray.m
+++ Helpers/HLPArray.m
@@ -65,29 +65,44 @@
 - (id)objectAtIndex:(NSUInteger)index {
     [self.backingStore compact];
     
-    return nil;
+    id object = (__bridge id)[self.backingStore pointerAtIndex:index];
+    return object;
 }
 
 #pragma mark - Mutable array
 
 - (void)insertObject:(id)anObject atIndex:(NSUInteger)index {
     [self.backingStore compact];
+    
+    void *pointer = (__bridge void *)anObject;
+    [self.backingStore insertPointer:pointer atIndex:index];
 }
 
 - (void)removeObjectAtIndex:(NSUInteger)index {
     [self.backingStore compact];
+    
+    [self.backingStore removePointerAtIndex:index];
 }
 
 - (void)addObject:(id)anObject {
     [self.backingStore compact];
+    
+    void *pointer = (__bridge void *)anObject;
+    [self.backingStore addPointer:pointer];
 }
 
 - (void)removeLastObject {
     [self.backingStore compact];
+    
+    NSUInteger index = self.backingStore.count - 1;
+    [self.backingStore removePointerAtIndex:index];
 }
 
 - (void)replaceObjectAtIndex:(NSUInteger)index withObject:(id)anObject {
     [self.backingStore compact];
+    
+    void *pointer = (__bridge void *)anObject;
+    [self.backingStore replacePointerAtIndex:index withPointer:pointer];
 }
 
 @end
