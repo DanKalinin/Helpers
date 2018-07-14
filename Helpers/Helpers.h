cd1c099351a53b19911315a072615d0061099eb6
Dan Kalinin <daniil551@hotmail.com>
2018-07-14 13:06:59 +0300

    Public header

--- Helpers/Helpers.h
+++ Helpers/Helpers.h
@@ -13,8 +13,8 @@ FOUNDATION_EXPORT const unsigned char HelpersVersionString[];
 
 #import <Helpers/HLPMain.h>
 #import <Helpers/HLPObject.h>
-#import <Helpers/HLPSet.h>
 #import <Helpers/HLPString.h>
+#import <Helpers/HLPSet.h>
 #import <Helpers/HLPArray.h>
 #import <Helpers/HLPDictionary.h>
 #import <Helpers/HLPOperation.h>
