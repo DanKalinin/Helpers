dafac3ab5daf3fcd09acc5a08a632600e1b893ef
Dan Kalinin <daniil551@hotmail.com>
2018-10-03 13:23:01 +0300

    Exception

---
+++ Helpers/HLPException.h
@@ -0,0 +1,16 @@
+//
+//  HLPException.h
+//  Helpers
+//
+//  Created by Dan Kalinin on 10/3/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HLPException : NSException
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Helpers/HLPException.m
@@ -0,0 +1,12 @@
+//
+//  HLPException.m
+//  Helpers
+//
+//  Created by Dan Kalinin on 10/3/18.
+//
+
+#import "HLPException.h"
+
+@implementation HLPException
+
+@end
